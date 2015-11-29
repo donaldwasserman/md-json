77476ce5a96dca9eabf480ade3050277e78ef744
Donald Wasserman <djwasserman@gmail.com>
2015-11-28 19:39:04 -0500

    First commit.

---
+++ index.js
@@ -0,0 +1,38 @@
+var readFile = require('./lib/readFile');
+var fs = require('fs');
+var pluralize = require('pluralize');
+
+module.exports = function(dir, fileName) {
+
+  return new Promise(function(resolve, reject) {
+    var folder = dir.toString();
+
+    if (typeof fileName === 'undefined') {
+      console.log(' Uh oh, hotdog');
+      fs.readdir(dir, function(err, files) {
+        if (!err) {
+          var posts = [];
+
+          files.forEach(function(e) {
+            posts.push(readFile(e, dir));
+          });
+          var data = {};
+          data[folder] = posts;
+
+         resolve(data);
+       } else {
+         reject(err);
+       }
+      });
+
+    } else {
+      var title = fileName+'.md';
+      var single = pluralize(folder, 1);
+      var data = {};
+      data[single] = readFile(title, dir);
+      console.log(data);
+      resolve(data);
+    }
+  });
+
+};
